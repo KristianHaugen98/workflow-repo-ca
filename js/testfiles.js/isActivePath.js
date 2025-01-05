28e683e6e55ac747b9ae1abfb17794e9f39505d7
KristianHaugen98 <kristian-haugen21@live.no>
2025-01-05 13:07:41 +0100

    Satt opp nødvendig forbredelser
    * Workflow branch
    * Utviklingsverktøy
    * Testet isActivePath funksjon

---
+++ js/testfiles.js/isActivePath.js
@@ -0,0 +1,4 @@
+// Definerer og eksporterer funsjonen
+export default function isActivePath(currentPath, href) {
+  return currentPath === href; // Sammenligner currentPatch med href og returnerer true eller false
+}
---
+++ js/testfiles.js/isActivePath.test.js
@@ -0,0 +1,17 @@
+import { describe, it, expect } from "vitest";
+import isActivePath from "./isActivePath.js";
+
+// Gruppere relaterte tester:
+
+describe("isActivePath", () => {
+  // Dette spesiferer en spesifik test:
+  it("should return true when currentPath matches href", () => {
+    const currentPath = "/home";
+    const href = "/home";
+
+    const result = isActivePath(currentPath, href);
+
+    // Sjekker forventet resultat:
+    expect(result).toBe(true);
+  });
+});
